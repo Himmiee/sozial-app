5976a794b6818d7abe0fa5eba29dff89d4e49f6e
Haliyah Tijani <haliyya460@gmail.com>
2023-05-24 17:56:49 +0100

    fix

--- src/app/pages/network/network.component.ts
+++ src/app/pages/network/network.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { profileService } from 'src/app/Services/profile.service';
+import {profileService } from "../../Services/profile.service"
 
 @Component({
   selector: 'app-network',
   templateUrl: './network.component.html',
-  styleUrls: ['./network.component.css']
+  styleUrls: ['./network.component.css'],
 })
 export class NetworkComponent implements OnInit {
   items: any[] = [];
-  faPlus = faPlus
-  constructor(private profile: profileService) { }
+  faPlus = faPlus;
+  loading = false;
+  addButtonLabel = 'Add Friend';
+  constructor(private profile: profileService) {}
 
   ngOnInit(): void {
-    this.profile.getFriends().subscribe((data:any) => this.items = data)
+    this.profile.getFriends().subscribe((data: any) => (this.items = data));
   }
 
+  setPending(id: string): void {
+
+    this.items.map((item: any) => {
+      if (item.id === id){
+      // this.addButtonLabel = 'Pending...';
+     }
+    })
+
+  }
 }
